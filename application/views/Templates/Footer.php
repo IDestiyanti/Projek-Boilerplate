389d822c0082d1fbe6ef2755728289d04d064037
IDestiyanti <intandesti25@gmail.com>
2020-09-05 21:39:40 +0700

    'upload'

---
+++ application/views/Templates/Footer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<script type="text/javascript" src="<?= base_url()?>assets/dist/js/bootstrap.bundle.js"></script>
+	<script type="text/javascript" src="<?= base_url()?>assets/dist/js/bootstrap.bundle.js.map"></script>
+	<script type="text/javascript" src="<?= base_url()?>assets/dist/js/bootstrap.bundle.min.js"></script>
+	<script type="text/javascript" src="<?= base_url()?>assets/dist/js/bootstrap.bundle.min.js.map"></script>
+	<script type="text/javascript" src="<?= base_url()?>assets/dist/js/bootstrap.js"></script>
+	<script type="text/javascript" src="<?= base_url()?>assets/dist/js/bootstrap.js.map"></script>
+	<script type="text/javascript" src="<?= base_url()?>assets/dist/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="<?= base_url()?>assets/dist/js/bootstrap.min.js.map"></script>
+	<script type="text/javascript" src="<?= base_url()?>assets/assets/js/vendor/jquery-slim.min.js"></script>
+	<script type="text/javascript" src="<?= base_url()?>assets/assets/js/vendor/popper.min.js"></script>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ application/views/Templates/Header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/dist/css/bootstrap-grid.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/dist/css/bootstrap-grid.css.map">
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/dist/css/bootstrap-grid.min.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/dist/css/bootstrap-grid.min.css.map">
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/dist/css/bootstrap-reboot.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/dist/css/bootstrap-reboot.css.map">
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/dist/css/bootstrap-reboot.min.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/dist/css/bootstrap-reboot.min.css.map">
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/dist/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/dist/css/bootstrap.min.css.map">
+	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/dist/css/bootstrap.css.map">
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
