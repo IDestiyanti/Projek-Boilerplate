e22cb4118710672944224e13b3d64252565efc37
IDestiyanti <intandesti25@gmail.com>
2020-09-06 00:08:19 +0700

    update views

--- application/controllers/Dosen/Dashboard.php
+++ application/controllers/Dosen/Dashboard.php
@@ -7,6 +7,9 @@ Class Dashboard extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('index');
+		$this->load->view('Templates/Header');
+		$this->load->view('Dosen/index');
+		$this->load->view('Templates/Footer');
 	}
 }
+?>
--- application/controllers/Mahasiswa/Dashboard.php
+++ application/controllers/Mahasiswa/Dashboard.php
@@ -7,6 +7,9 @@ Class Dashboard extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('index');
+		$this->load->view('Templates/Header');
+		$this->load->view('Mahasiswa/index');
+		$this->load->view('Templates/Footer');
 	}
 }
+?>
